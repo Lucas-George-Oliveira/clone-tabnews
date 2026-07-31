6aaf2b7940bd814c75adb1ddbda891fdff13e859
Lucas George <lucasgeorge1110@gmail.com>
2026-07-31 19:45:26 +0000

    adiciona scripts `lint:check` e `lint:fix`

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,8 @@
 function Home() {
-  return <h1>Eu amo vocês do curso.dev.</h1>
+  return <h1>Eu amo vocês do curso.dev.</h1>;
 }
 
+function teste() {
+  console.log("teste");
+}
 export default Home;
